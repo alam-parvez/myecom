7acf99344c0652fd9b13a6b3c389af72bb9c2753
alam-parvez <parvezalam25677@gmail.com>
2025-03-04 23:23:56 +0530

    second commit

---
+++ database/migrations/2025_03_04_172004_create_products_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->require();
+            $table->string("color,20")->require();
+            $table->string("size,20")->require();
+            $table->integer("basePrice",)->require();
+            $table->integer("discount",)->require();
+            $table->integer("finalPrice",)->require();
+            $table->boolean("stock")->nullable(true)->default(true);
+            $table->integer("stockQuantity",)->require();
+            $table->text("description",)->require();
+            $table->boolean("active")->nullable(true)->default(true);
+            
+            $table->bigInteger("maincategory_id")->unsigned()->index();
+            $table->foreign("maincategory_id")->references("id")->on("maincategories")->onDelete("cascade");
+
+            $table->bigInteger("subcategory_id")->unsigned()->index();
+            $table->foreign("subcategory_id")->references("id")->on("subcategories")->onDelete("cascade");
+            
+            $table->bigInteger("brand_id")->unsigned()->index();
+            $table->foreign("brand_id")->references("id")->on("brands")->onDelete("cascade");
+             
+
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
