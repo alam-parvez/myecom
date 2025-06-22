2431a870bba80b62028dc0c887ee382945d9c1ee
alam-parvez <parvezalam25677@gmail.com>
2025-06-22 16:56:30 +0530

    push first

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -38,12 +38,6 @@
                                </div>
                             </div>
 
-
-
-
-
-
-
                         <div class="row">
                             <div class="col-md-6 mb-3">
                                 <label>Pic*</label>
